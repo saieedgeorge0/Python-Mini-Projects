9018c1eb6ac6b3d58473fb3e06fcbcc6e5f227e7
George Saieed <saieed.george0@gmail.com>
2016-05-11 13:40:03 -0500

    shapes update

--- Shapes/mindstorms.py
+++ Shapes/mindstorms.py
@@ -1,13 +1,25 @@
 import turtle
+import Tkinter as TK
 import _tkinter
 
-def draw_square():
+def draw_shapes():
     window = turtle.Screen()
     window.bgcolor("red")
 
     george = turtle.Turtle()
-    george.forward(100)
+    george.shape("turtle")
+    george.color("yellow")
+    george.speed(2)
+
+    for i in range(1:5):
+        george.forward(100)
+        george.right(90)
+
+    mary = turtle.Turtle()
+    mary.shape("arrow")
+    mary.color("blue")
+    mary.circle(100)
 
     window.exitonclick()
 
-draw_square()
+draw_shapes()
