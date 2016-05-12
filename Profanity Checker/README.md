64515d8acd7d55ae3f6c7b3d5b94cde872a870d2
George Saieed <saieed.george0@gmail.com>
2016-05-12 01:41:45 -0500

    finish

--- Profanity Checker/README.md
+++ Profanity Checker/README.md
@@ -1,2 +1,2 @@
 #Profanity-Checker
-Reads a txt file and checks for the presence of any profane words, replacing all the letters but the first letter of each profanity with ***s.
+Reads a txt file and checks for the presence of any profane words, informing you if it has found any.
--- Profanity Checker/profanity_checker.py
+++ Profanity Checker/profanity_checker.py
@@ -1,6 +1,16 @@
+import urllib
+
 def read_text():
     email = open("email.txt")
     contents = email.read()
     print(contents)
     email.close()
+    check_profanity(contents)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://wdyl.com/profanity?q=" + text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+
 read_text()
